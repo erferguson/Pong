3970432495331b875dc0c0920c708d079675255a
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-09-07 12:24:57 -0700

    turtle imported and basic turle setup

--- Pong1.py
+++ Pong1.py
@@ -1,1 +1,10 @@
 # Simple Pong in Python3
+
+import turtle  # built-in Python, starter for Games, good for Beginners
+
+wn = turtle.Screen()
+wn.title('Pong by ERFerguson')
+wn.bgcolor('black')
+wn.setup(width=800, height=600)
+wn.tracer(0) # stops the window from Updating, means a Manual Update now
+
