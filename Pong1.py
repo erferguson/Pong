4f55a1cdecdbab7a7dced93fa9011b959fe3f176
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-09-07 13:24:30 -0700

    scoreboard / pen > created and tested

--- Pong1.py
+++ Pong1.py
@@ -8,11 +8,13 @@ wn.bgcolor('black')
 wn.setup(width=800, height=600)
 wn.tracer(0) # stops the window from Updating, means a Manual Update now
 
+
+
 # Paddle A > on LEFT
 a_paddle = turtle.Turtle()
 a_paddle.speed(0)
 a_paddle.shape('square')
-a_paddle.color('yellow')
+a_paddle.color('alice blue')
 a_paddle.shapesize(stretch_wid=5, stretch_len=1)
 a_paddle.penup()
 a_paddle.goto(-350, 0) # set starting position of a_paddle
@@ -21,7 +23,7 @@ a_paddle.goto(-350, 0) # set starting position of a_paddle
 b_paddle = turtle.Turtle()
 b_paddle.speed(0)
 b_paddle.shape('square')
-b_paddle.color('gray')
+b_paddle.color('light slate gray')
 b_paddle.shapesize(stretch_wid=5, stretch_len=1)
 b_paddle.penup()
 b_paddle.goto(350, 0) # set starting position of b_paddle
@@ -30,11 +32,21 @@ b_paddle.goto(350, 0) # set starting position of b_paddle
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape('circle')
-ball.color('green')
+ball.color('lime')
 ball.penup()
 ball.goto(0, 0) # set starting position of ball
 ball.dx = 2 #delta ?
 ball.dy = -2 # everytime ball moves it moves 2 pixles
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color('hotpink')
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write('Player 1: 0  Player 2: 0', align='center', font=('Courier', 24, 'normal'))
+
 
 # Function > A_Paddle UP
 def a_paddle_up():
